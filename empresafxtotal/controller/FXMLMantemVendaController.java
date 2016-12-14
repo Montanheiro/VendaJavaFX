44b0fe3592e5b1c047b9ca96bca717cc5ba4d588
Montanheiro <lucasmontanheiro10@gmail.com>
2016-12-14 15:50:53 -0200

    Puxando vendendor, cliente e produtos

--- empresafxtotal/controller/FXMLMantemVendaController.java
+++ empresafxtotal/controller/FXMLMantemVendaController.java
@@ -1,7 +1,15 @@
 package empresafxtotal.controller;
 
+import empresafxtotal.model.ClienteDAO;
+import empresafxtotal.model.FuncionarioDAO;
+import empresafxtotal.model.ProdutoDAO;
 import java.net.URL;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
@@ -17,22 +25,42 @@ public class FXMLMantemVendaController implements Initializable {
     private TextField textFieldPreco;
 
     @FXML
-    private ComboBox<?> comboBoxProdutos;
+    private ComboBox<Produto> comboBoxProdutos;
 
     @FXML
     private TextField textFieldQuantidade;
 
     @FXML
-    private ComboBox<?> comboBoxVendedor;
+    private ComboBox<Funcionario> comboBoxVendedor;
 
     @FXML
-    private ComboBox<?> comboBoxCliente;
+    private ComboBox<Cliente> comboBoxCliente;
 
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         System.out.println("ABRIU CARARIO");
         
+        try {
+            List<Cliente> clientes = ClienteDAO.retreaveAll();
+            comboBoxCliente.getItems().addAll(clientes);
+        } catch (SQLException ex) {
+            Logger.getLogger(FXMLMantemVendaController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+             
+        try {
+            List<Funcionario> vendedores = FuncionarioDAO.retreaveAllVendedores();
+            comboBoxVendedor.getItems().addAll(vendedores);
+        } catch (SQLException ex) {
+            Logger.getLogger(FXMLMantemVendaController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        try {
+            List<Produto> produtos = ProdutoDAO.retreaveAll();
+            comboBoxProdutos.getItems().addAll(produtos);
+        } catch (SQLException ex) {
+            Logger.getLogger(FXMLMantemVendaController.class.getName()).log(Level.SEVERE, null, ex);
+        }
         
         
     }
